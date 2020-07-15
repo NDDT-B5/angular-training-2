8c05e401f8e0dbf4cab768471ee4afae311ccb75
Nicolas Torchalla <nicolas@torchalla.eu>
2020-07-15 23:59:19 +0200

    Dialogs

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -22,6 +22,8 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -32,7 +34,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -43,9 +46,13 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 	MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule
   ],
   providers: [DishService,PromotionService, LeaderService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    LoginComponent
+],
 })
 export class AppModule { }
